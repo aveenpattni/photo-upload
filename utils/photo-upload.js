548367942947fd3e06b537dec97759636198e303
Aveen Pattni <aveenpattni@gmail.com>
2021-03-23 12:34:14 -0400

    update variables

--- utils/photo-upload.js
+++ utils/photo-upload.js
@@ -3,7 +3,7 @@ const aws = require("aws-sdk");
 const config = require("../config");
 
 aws.config.update({
-  accessKeyId:"",
-  secretAccessKey: "",
+  accessKeyId: config.AWS_ACCESS_KEY,
+  secretAccessKey: config.AWS_SECRET_ACCESS,
   region: config.REGION
 })
