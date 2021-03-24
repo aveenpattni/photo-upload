eea9e33c6ea6b9365843f9278f9944b598508be7
Aveen Pattni <aveenpattni@gmail.com>
2021-03-24 10:58:35 -0400

    update names

--- utils/photo-upload.js
+++ utils/photo-upload.js
@@ -3,6 +3,7 @@ const multer = require("multer");
 const multerS3 = require("multer-s3");
 
 const config = require("../config");
+const user = "avp"
 
 aws.config.update({
   accessKeyId: config.AWS_ACCESS_KEY,
@@ -16,6 +17,7 @@ const fileFilter = (req, file, cb) => {
   if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
     cb(null, true);
   } else {
+    console.log("🔥", new Error("Invalid File type. Only JPEG and PNG"));
     cb(new Error("Invalid File type. Only JPEG and PNG"), false);
   }
 }
@@ -26,11 +28,12 @@ const imageUpload = multer({
     s3,
     bucket: "aveen-test-s3/photo-upload",
     acl: "public-read",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
     metadata: function (req, file, cb) {
-      cb(null, {fieldName: "Metadata_Test"});
+      cb(null, {fieldName: "photo-upload-node"});
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString());
+      cb(null, `${user}_${Date.now().toString()}`);
     }
   })
 })
